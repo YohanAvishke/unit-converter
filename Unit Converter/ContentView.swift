67bbdb1d5d1581ae8c311bac3cf1d5890f00a97d
Yohan Avishke <yaediriweera@gmail.com>
2021-02-25 23:52:14 +0530

    Initial Commit

---
+++ Unit Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Converter
+//
+//  Created by Yohan Avishke Ediriweera on 2021-02-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Yohan Avishke Ediriweera on 2021-02-25.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
