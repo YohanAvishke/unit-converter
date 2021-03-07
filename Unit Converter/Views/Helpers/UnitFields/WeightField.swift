80b5eaf680dfe812834cd017768a211d365a8e21
Yohan Avishke <yaediriweera@gmail.com>
2021-03-07 10:28:53 +0530

    Reformat functions in weight-form

--- Unit Converter/Views/Helpers/UnitFields/WeightField.swift
+++ Unit Converter/Views/Helpers/UnitFields/WeightField.swift
@@ -5,6 +5,74 @@ struct WeightField: View {
     @State private var editor: WeightEditor = WeightEditor()
     @Binding var valueOfWeight: ValueOfWeight
     
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10.0) {
+            switch unitType {
+            case .kilogram:
+                Text("Kilograms")
+                createTextField(placeholder: "kg",
+                                binding: $valueOfWeight.kilogram
+                )
+                .onChange(of: valueOfWeight.kilogram) { (value) in
+                    if editor.kilogram {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            case .gram:
+                Text("Grams")
+                createTextField(placeholder: "g",
+                                binding: $valueOfWeight.gram
+                )
+                .onChange(of: valueOfWeight.gram) { (value) in
+                    if editor.gram {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            case .ounce:
+                Text("Ounces")
+                createTextField(placeholder: "ou",
+                                binding: $valueOfWeight.ounce
+                )
+                .onChange(of: valueOfWeight.ounce) { (value) in
+                    if editor.ounce {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            case .pound:
+                Text("Pounds")
+                createTextField(placeholder: "lb",
+                                binding: $valueOfWeight.pound
+                )
+                .onChange(of: valueOfWeight.pound) { (value) in
+                    if editor.pound {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            case .stone:
+                Text("Stones")
+                createTextField(placeholder: "st",
+                                binding: $valueOfWeight.stone
+                )
+                .onChange(of: valueOfWeight.stone) { (value) in
+                    if editor.stone {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            case .stone_pound:
+                Text("Pounds")
+                createTextField(placeholder: "lb",
+                                binding: $valueOfWeight.stonePound
+                )
+                .onChange(of: valueOfWeight.stonePound) { (value) in
+                    if editor.stonePound {
+                        onUnitEdited(changedValue: value)
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 15)
+    }
+    
     func createTextField(placeholder: String, binding: Binding<String>) -> AnyView {
         return AnyView(
             TextField (
@@ -79,75 +147,6 @@ struct WeightField: View {
             valueOfWeight.pound = converter.convert(value: changedValue, to: .pound)
             valueOfWeight.stone = converter.convert(value: changedValue, to: .stone)
         }
-    }
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10.0) {
-            
-            switch unitType {
-            case .kilogram:
-                Text("Kilograms")
-                createTextField(placeholder: "kg",
-                                binding: $valueOfWeight.kilogram
-                )
-                .onChange(of: valueOfWeight.kilogram) { (value) in
-                    if editor.kilogram {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            case .gram:
-                Text("Grams")
-                createTextField(placeholder: "g",
-                                binding: $valueOfWeight.gram
-                )
-                .onChange(of: valueOfWeight.gram) { (value) in
-                    if editor.gram {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            case .ounce:
-                Text("Ounces")
-                createTextField(placeholder: "ou",
-                                binding: $valueOfWeight.ounce
-                )
-                .onChange(of: valueOfWeight.ounce) { (value) in
-                    if editor.ounce {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            case .pound:
-                Text("Pounds")
-                createTextField(placeholder: "lb",
-                                binding: $valueOfWeight.pound
-                )
-                .onChange(of: valueOfWeight.pound) { (value) in
-                    if editor.pound {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            case .stone:
-                Text("Stones")
-                createTextField(placeholder: "st",
-                                binding: $valueOfWeight.stone
-                )
-                .onChange(of: valueOfWeight.stone) { (value) in
-                    if editor.stone {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            case .stone_pound:
-                Text("Pounds")
-                createTextField(placeholder: "lb",
-                                binding: $valueOfWeight.stonePound
-                )
-                .onChange(of: valueOfWeight.stonePound) { (value) in
-                    if editor.stonePound {
-                        onUnitEdited(changedValue: value)
-                    }
-                }
-            }
-        }
-        .padding(.horizontal, 15)
     }
 }
 
