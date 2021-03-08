ed6800af243d42914209070fa104e7af6614486c
Yohan Avishke <yaediriweera@gmail.com>
2021-03-08 12:11:58 +0530

    Add tab navigation to history view

--- Unit Converter/Views/History/HistoryView.swift
+++ Unit Converter/Views/History/HistoryView.swift
@@ -1,8 +1,59 @@
 import SwiftUI
 
 struct HistoryView: View {
+    @State var index = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            // Tab view
+            HStack(spacing: 0){
+                Text("Day")
+                    .foregroundColor(index == 0 ? .white : Color(.black).opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color(.blue).opacity(index == 0 ? 1 : 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        index = 0
+                    }
+                
+                Spacer(minLength: 0)
+                
+                Text("Week")
+                    .foregroundColor(index == 1 ? .white : Color(.black).opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color(.blue).opacity(index == 1 ? 1 : 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        index = 1
+                    }
+                
+                Spacer(minLength: 0)
+                
+                Text("Month")
+                    .foregroundColor(index == 2 ? .white : Color(.black).opacity(0.7))
+                    .fontWeight(.bold)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 35)
+                    .background(Color(.blue).opacity(index == 2 ? 1 : 0))
+                    .clipShape(Capsule())
+                    .onTapGesture {
+                        index = 2
+                    }
+            }
+            .background(Color.black.opacity(0.06))
+            .clipShape(Capsule())
+            .padding(.horizontal)
+            .padding(.top, 25)
+            
+            // Dashboard Grid
+            
+            Spacer(minLength: 0)
+        }
+        .padding(.top)
     }
 }
 
