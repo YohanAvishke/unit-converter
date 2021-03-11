61b78ed0d0fcf6fbfb8a772145e481bd32e0e95a
Yohan Avishke <yaediriweera@gmail.com>
2021-03-12 00:54:34 +0530

    Add converter types

---
+++ UnitConverter/Controllers/ConvertersSceneController.swift
@@ -0,0 +1,35 @@
+//
+//  ConvertersSceneController.swift
+//  UnitConverter
+//
+//  Created by Yohan Avishke Ediriweera on 2021-03-12.
+//
+
+import Foundation
+import UIKit
+
+class ConvertersSceneController: UIViewController,
+                                 UICollectionViewDataSource,
+                                 UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return converters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ConverterViewCell
+        cell.conversionName.text = converters[indexPath.row].name
+        cell.conversionIcon.image = converters[indexPath.row].image
+        
+        //Card(cell) styles
+        cell.contentView.layer.cornerRadius = 10.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.00).cgColor
+        cell.contentView.layer.masksToBounds = false
+        
+        return cell
+    }
+    
+}
---
+++ UnitConverter/Models/ConverterViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ConverterViewCell.swift
+//  UnitConverter
+//
+//  Created by Yohan Avishke Ediriweera on 2021-03-12.
+//
+
+import Foundation
+import UIKit
+
+class ConverterViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var conversionIcon: UIImageView!
+    @IBOutlet weak var conversionName: UILabel!
+}
