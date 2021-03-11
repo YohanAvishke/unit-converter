804358937588f9f6881b2daf8e76b96fd9401790
Yohan Avishke <yaediriweera@gmail.com>
2021-03-11 15:43:09 +0530

    Add keyboard views

---
+++ Unit Converter/Views/Helpers/Keyboard/KeyPadRow.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct KeyPadRow: View {
+    var keys: [String]
+    
+    var body: some View {
+        HStack {
+            ForEach(keys, id: \.self) { key in
+                KeyPadButton(key: key)
+            }
+        }
+    }
+}
