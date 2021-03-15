1d8547dcca3d55d4fbb74e24b6e986b21a510ddc
Yohan Avishke <yaediriweera@gmail.com>
2021-03-15 21:08:43 +0530

    Add constanst file

---
+++ UnitConverter/Utils/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  UnitConverter
+//
+//  Created by Yohan Avishke Ediriweera on 2021-03-15.
+//
+
+import Foundation
+
+struct HistoryConst {
+    static let MAX_SIZE = 5
+    
+    static let WEIGHT_KEY = "weight"
+    static let TEMP_KEY = "temperature"
+    static let DISTANCE_KEY = "distance"
+    static let SPEED_KEY = "speed"
+    static let VOLUME_KEY = "volume"
+}
